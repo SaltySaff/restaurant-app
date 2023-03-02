b40880785913245bbd9f2a2dc63418e5d30d2df6
saltysaff <d_ash12345@hotmail.co.uk>
2023-03-02 19:59:13 +0000

    added data and other misc tasks

---
+++ data.js
@@ -0,0 +1,156 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        emoji: "🍕"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1
+    },
+    {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🍺",
+        id: 2
+    },
+    {
+        name: "Sushi",
+        ingredients: ["rice", "raw fish", "nori"],
+        price: 20,
+        emoji: "🍣",
+        id: 3
+    },
+    {
+        name: "Taco",
+        ingredients: ["beef", "lettuce", "tomato", "sour cream"],
+        price: 8,
+        emoji: "🌮",
+        id: 4
+    },
+    {
+        name: "Pad Thai",
+        ingredients: ["rice noodles", "chicken", "tofu", "peanuts"],
+        price: 16,
+        emoji: "🍜",
+        id: 5
+    },
+    {
+        name: "Fried Rice",
+        ingredients: ["rice", "egg", "carrots", "peas", "soy sauce"],
+        price: 10,
+        emoji: "🍚",
+        id: 6
+    },
+    {
+        name: "Caesar Salad",
+        ingredients: ["romaine lettuce", "parmesan cheese", "croutons"],
+        price: 9,
+        emoji: "🥗",
+        id: 7
+    },
+    {
+        name: "Pesto Pasta",
+        ingredients: ["pasta", "basil", "pine nuts", "garlic", "parmesan cheese"],
+        price: 13,
+        emoji: "🍝",
+        id: 8
+    },
+    {
+        name: "Chicken Parmesan",
+        ingredients: ["chicken breast", "tomato sauce", "mozzarella cheese", "breadcrumbs", "spaghetti"],
+        price: 18,
+        emoji: "🍗",
+        id: 9
+    },
+    {
+        name: "Fish and Chips",
+        ingredients: ["cod", "potatoes", "flour", "beer"],
+        price: 15,
+        emoji: "🐟",
+        id: 10
+    },
+    {
+        name: "Pho",
+        ingredients: ["beef broth", "rice noodles", "beef", "bean sprouts", "lime"],
+        price: 12,
+        emoji: "🍲",
+        id: 11
+    },      
+    {
+        name: "Steak Frites",
+        ingredients: ["steak", "french fries", "maitre d'hotel butter"],
+        price: 24,
+        emoji: "🥩",
+        id: 12
+    },
+    {
+        name: "Katsu Curry",
+        ingredients: ["chicken", "rice", "curry sauce", "panko breadcrumbs"],
+        price: 16,
+        emoji: "🍛",
+        id: 13
+    },
+    {
+        name: "Falafel Wrap",
+        ingredients: ["falafel", "pita bread", "hummus", "lettuce", "tomato"],
+        price: 10,
+        emoji: "🌯",
+        id: 14
+    },
+    {
+        name: "Butter Chicken",
+        ingredients: ["chicken", "tomato sauce", "cream", "rice"],
+        price: 15,
+        emoji: "🍛",
+        id: 15
+    },
+    {
+        name: "Beef Stroganoff",
+        ingredients: ["beef", "sour cream", "mushrooms", "egg noodles"],
+        price: 18,
+        emoji: "🥩",
+        id: 16
+    },
+    {
+        name: "Lobster Roll",
+        ingredients: ["lobster meat", "butter", "lemon", "roll"],
+        price: 22,
+        emoji: "🦞",
+        id: 17
+    },
+    {
+        name: "Gnocchi",
+        ingredients: ["potatoes", "flour", "egg", "tomato sauce"],
+        price: 14,
+        emoji: "🍝",
+        id: 18
+    },
+    {
+        name: "Fish Tacos",
+        ingredients: ["fish", "tortillas", "cabbage", "avocado", "salsa"],
+        price: 12,
+        emoji: "🌮",
+        id: 19
+    },
+    {
+        name: "Pork Belly Bao",
+        ingredients: ["pork belly", "bao bun", "hoisin sauce", "cucumber"],
+        price: 10,
+        emoji: "🥟",
+        id: 20
+    },
+    {
+        name: "Shakshuka",
+        ingredients: ["eggs", "tomatoes", "bell peppers", "onion", "feta cheese"],
+        price: 13,
+        emoji: "🍳",
+        id: 21
+    }
+];
