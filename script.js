7491fc3c67f18904ebdda5cc27318cf01a5699a0
saltysaff <d_ash12345@hotmail.co.uk>
2023-03-03 15:38:26 +0000

    finish layout

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ import { menuArray } from "./data.js";
 
 console.log(menuArray)
 
+const emojiButton = new EmojiButton()
+
+const emoji = '🍕'
+const emojiMarkup = emojiButton.render(emoji);
+document.querySelector('#emoji').innerHTML = emojiMarkup;
+
+
